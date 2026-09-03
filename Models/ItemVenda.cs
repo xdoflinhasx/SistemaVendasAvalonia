d0634404e3e0437bab02862c561d0cbfcc37cb61
xdoflinhasx <xdoflinhasx@users.noreply.github.com>
2026-09-03 19:30:23 -0300

    Refresh grid after editing sale

--- Models/ItemVenda.cs
+++ Models/ItemVenda.cs
@@ -1,8 +1,9 @@
+using System.ComponentModel;
 using System.Globalization;
 
 namespace MeuAppAvalonia;
 
-public sealed class ItemVenda
+public sealed class ItemVenda : INotifyPropertyChanged
 {
     public int Id { get; private set; }
     public string Codigo { get; private set; } = string.Empty;
@@ -11,6 +12,8 @@ public sealed class ItemVenda
     public int Quantidade { get; private set; }
     public string TextoPreco => Preco.ToString("C2", CultureInfo.CurrentCulture);
     public decimal Total => Preco * Quantidade;
+
+    public event PropertyChangedEventHandler? PropertyChanged;
 
     private ItemVenda()
     {
@@ -30,5 +33,11 @@ public sealed class ItemVenda
         Nome = nome;
         Preco = preco;
         Quantidade = quantidade;
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Codigo)));
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Nome)));
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Preco)));
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Quantidade)));
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(TextoPreco)));
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Total)));
     }
 }
